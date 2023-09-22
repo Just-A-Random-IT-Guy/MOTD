f42e605aaa63a42e10b8e5af6b93cca2b234cef2
Just A Random IT Guy <justarandomitguy@gmail.com>
2023-09-22 20:43:58 +0100

    Added a function to request a random joke from the JokeAPI @ https://v2.jokeapi.dev/joke/, function will use a random joke from a list in the script iif JokeAPI returns null

--- ubuntu_motd_custom.sh
+++ ubuntu_motd_custom.sh
@@ -1,12 +1,55 @@
 #!/bin/bash
 
 
+# Function to get a random joke using Python
+get_random_joke() {
+    python3 <<EOF
+import requests
+import random
+
+# List of jokes
+jokes = [
+    "Why do programmers prefer dark mode? Because the light attracts bugs!",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Parallel lines have so much in common... It's a shame they'll never meet.",
+    "Why don't skeletons fight each other? They don't have the guts!",
+    "What do you call fake spaghetti? An impasta!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "What did one ocean say to the other ocean? Nothing, they just waved."
+]
+
+def get_random_joke():
+    # Flags to avoid requesting rude &/or crude jokes, feel free to remove flags if you like!
+    # NOTE: Documentation on how to use the JokeApi can be found @ https://v2.jokeapi.dev/joke/
+    flags = ["nsfw", "religious", "racist", "sexist", "explicit"]
+    
+    # Fetch a joke from JokeAPI with specified flags
+    response = requests.get(f"https://v2.jokeapi.dev/joke/Any?blacklistFlags={','.join(flags)}")
+    
+    if response.status_code == 200:
+        data = response.json()
+        if data['type'] == 'single':
+            joke = data['joke']
+            if joke and len(joke) >= 5:
+                return joke
+    
+    # If JokeAPI did not return a valid joke, select a random joke from the list
+    return random.choice(jokes)
+
+random_joke = get_random_joke()
+print(random_joke)
+EOF
+}
+
+
+
 echo "#############################################"
 echo "Unauthorized access to this system is strictly prohibited. Only authorized users are allowed. Any malicious or unauthorized access will be reported to the authorities."
 echo "#############################################"
 
 # Current Date & Time
 echo "Current Date & Time: $(date)"
+# I used the following online tool for generating the following ASCII art - https://onlinetools.com/ascii/convert-text-to-ascii-art 
 echo "#############################################"
 echo "____    __    ____  _______  __        ______   ______   .___  ___.  _______    .______        ___       ______  __  ___"
 echo "\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|   |   _  \\      /   \\     /      ||  |/  /"
@@ -27,7 +70,6 @@ echo "Local IP address: $(hostname -I)"
 echo "Public IP address: $(curl -s ifconfig.me)"
 echo "#############################################"
 
-
 # Disk Usage
 echo "Disk Usage:"
 df -h
@@ -41,3 +83,11 @@ echo "#############################################"
 # Last login time
 echo "Last login: $(last -1 -F | awk 'NR==1{print $1" logged in on "$5" "$6" "$7" from "$3}')"
 echo "#############################################"
+
+# Display a random joke
+echo "#############################################"
+echo "Random Joke:"
+get_random_joke
+echo "#############################################"
+
+
